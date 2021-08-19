e0838c2e0ab9646913856fe34198b1705efa2fc2
Carlos Guilherme <52669963+cgmoraes@users.noreply.github.com>
2021-08-19 14:50:59 -0300

    Enunciado

--- ArvBB_PesqInterna.c
+++ ArvBB_PesqInterna.c
@@ -1,3 +1,29 @@
+/*• Aten¸c˜ao:
+1. E/S: tanto a entrada quanto a sa´ıda de dados devem ser “secas”, ou seja, n˜ao devem
+apresentar frases explicativas.
+2. Identificadores de vari´aveis: escolha nomes apropriados.
+3. Documenta¸c˜ao: inclua cabe¸calho, coment´arios e indenta¸c˜ao no programa.
+• Descri¸c˜ao:
+Implemente as opera¸c˜oes b´asicas de um algoritmo de pesquisa para Arvores Bin´arias de Busca ´
+conforme as especifica¸c˜oes abaixo. Escreva um programa que contenha os procedimentos a
+seguir: (1) inicializa¸c˜ao, (2) pesquisa, (3) inser¸c˜ao e (4) remo¸c˜ao. N˜ao ´e permitido o uso de
+algoritmos de ordena¸c˜ao para manter os dados ordenados, caso seja necess´ario. Na opera¸c˜ao
+de remo¸c˜ao, se necess´ario, dˆe a preferˆencia para a promo¸c˜ao da menor chave da sub´arvore `a
+direita, ou seja, o sucessor.
+• Entrada:
+A primeira linha da entrada consiste de uma sequˆencia de n´umeros inteiros positivos separados
+por espa¸cos. A sequˆencia termina quando for digitado um n´umero inteiro negativo. A linha
+seguinte cont´em um n´umero inteiro positivo que ser´a utilizado como chave a ser pesquisada
+no dicion´ario. No caso de uma pesquisa sem sucesso, esse n´umero dever´a ser inserido no
+dicion´ario, caso contr´ario, ele dever´a ser removido do dicion´ario.
+• Sa´ıda:
+Imprima duas linhas, cada qual com dois n´umeros inteiros, onde o primeiro valor representa
+tamanho do dicion´ario de dados, ou seja, a quantidade de dados armazenados, e o segundo
+valor deve exibir a altura m´axima da ´arvore a partir da raiz. A primeira linha exibe a
+quantidade de elementos e altura da ´arvore ap´os a inser¸c˜ao dos dados, e a segunda linha deve
+exibir as mesmas informa¸c˜oes ap´os a pesquisa, oque poder´a causar aumento ou diminui¸c˜ao
+na quantidade de elementos e/ou na altura da ´arvore.*/
+
 #include "stdio.h"
 #include "stdlib.h"
 
